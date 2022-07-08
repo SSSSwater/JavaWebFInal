d179ed716bc9ea79feeca434d0ec4415400edadc
朱宇杰 <>
2022-07-08 16:19:57 +0800

    新增三个exelget方法

--- src/main/java/com/example/jdbc/IloginDao.java
+++ src/main/java/com/example/jdbc/IloginDao.java
@@ -1,10 +1,20 @@
 package com.example.jdbc;
 
+import com.example.basicLayout.Manager;
+import com.example.basicLayout.Student;
+import com.example.basicLayout.Teacher;
+
 public interface IloginDao {
     boolean findStudentByAp(String account,String password);
 
     boolean findTeacherByAp(String account,String password);
 
     boolean findManagerByAp(String account,String password);
+
+    void updateStudent(Student students);
+
+    void updateTeacher(Teacher teachers);
+
+    void updateManager(Manager managers);
 }
 
