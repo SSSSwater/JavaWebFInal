f05ff870bdc1dbdb13fbab007e411598e7edf7de
tsl <1910228838@qq.com>
2022-07-12 16:26:08 +0800

    测试

--- src/main/webapp/js/showData.js
+++ src/main/webapp/js/showData.js
@@ -1,5 +1,6 @@
 function showDataHead(tDiv, queDiv, id, type, title, mode) {
     tDiv.id = 'div_' + id;
+    tDiv.id = 'div_' + id;
     tDiv.className = 'row m-3';
     queDiv.className = 'col-10';
     if (mode == 1) {
