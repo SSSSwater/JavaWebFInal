1d00f8b8604a889cffe7565df94099296308b8b8
theangrin <3050702029@qq.com>
2022-07-13 16:36:30 +0800

    所有界面在正确的网址下均可打开。 网页内容缺失问题待解决。

--- src/main/java/com/example/springboot/controller/PaperController.java
+++ src/main/java/com/example/springboot/controller/PaperController.java
@@ -1,10 +1,36 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.service.QuestionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @Controller
 public class PaperController {
+    @Autowired
+    QuestionService questionService;
+    @ResponseBody
+    @GetMapping(value = "/teacher/make_paper")
+    public Object makePaper(HttpServletRequest request){
+        log.info("获取题目");
+        int id=Integer.parseInt(request.getParameter("id"));
+        String subject=request.getParameter("subject");
+        String type=request.getParameter("type");
+        log.info(id+subject+type);
+        log.info(questionService.getQuestion(id,subject,type).toString());
+        return questionService.getQuestion(id,subject,type);
+    }
 
+    @RequestMapping("/teacher/make_paper.html")
+    public String doPaper(){
+        return "/teacher/make_paper";
+    }
 
 
 
