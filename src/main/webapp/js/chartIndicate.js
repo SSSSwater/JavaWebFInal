c3c59705fa53d2c2171fd3288453e984aa5d733a
SSwater <307722647@qq.com>
2022-07-13 14:10:22 +0800

    chart.js图表开工

---
+++ src/main/webapp/js/chartIndicate.js
@@ -0,0 +1,47 @@
+var classSelect = new Vue({
+    el: '#classSelect',
+    data() {
+        return {
+            options: [{
+                value: '选项1',
+                label: '黄金糕'
+            }, {
+                value: '选项2',
+                label: '双皮奶'
+            }],
+            value: ''
+        }
+    }
+})
+
+
+
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+};
+const classAverage = new Chart(
+    document.getElementById('classAverage'),
+    config
+);
